ddc2ef056788608f70086403a76dcc235a6d3681
Mukund Kapadia <mdrkkapadia@gmail.com>
2024-03-24 13:37:12 -0400

    index file done

---
+++ api/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const cors  = require('cors');
+require('dotenv').config();
+const mongoose = require('mongoose');
+const connectDatabase = require('./config/database');
+const registerUser = require('./controllers/UserController');
+const router = require('./routes/userRoutes');
+const cookieParser  = require('cookie-parser')
+
+connectDatabase();
+app.use(cors({
+    credentials:true,
+    origin:'http://localhost:5173'
+
+}));
+app.use('/uploads', express.static(__dirname+'/uploads'));
+
+app.use(cookieParser())
+app.use(express.json());
+
+app.use(router)
+
+app.listen(3000, (req, res) => {
+    console.log('Server listening on port 3000')
+})

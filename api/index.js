38ad6b9baaeb1543d1375e83521dd85a1ccccd37
Mukund Kapadia <mdrkkapadia@gmail.com>
2024-04-09 23:02:31 -0400

    bugs resolved

--- api/index.js
+++ api/index.js
@@ -11,30 +11,15 @@ const cookieParser  = require('cookie-parser')
 connectDatabase();
 app.use(cors({
     credentials:true,
-    origin:'https://luxereserve-hotel.onrender.com',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    origin:'http://localhost:5173'
 
 }));
-
-app.options('/places', (req, res) => {
-    res.header('Access-Control-Allow-Origin', 'https://luxereserve-hotel.onrender.com');
-    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.sendStatus(200);
-  });
-  
 app.use('/uploads', express.static(__dirname+'/uploads'));
 
 app.use(cookieParser())
 app.use(express.json());
 
 app.use(router)
-
-// app.use(cors({
-//     origin: 'http://localhost:5173',
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow PUT method
-//     allowedHeaders: ['Content-Type', 'Authorization'],
-//   }));
 
 app.listen(3000, (req, res) => {
     console.log('Server listening on port 3000')
